f17fece142afebf883cb11443c8eedb5ae680346
yuanxiaopeng <fishballian@qq.com>
2016-04-09 14:37:17 +0800

    ff

--- src/erlangmk_app.erl
+++ src/erlangmk_app.erl
@@ -1,17 +1,16 @@
 -module(erlangmk_app).
--behaviour(application).
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
--endif.
--export([start/2]).
--export([stop/1]).
 
-start(_Type, _Args) ->
-	erlangmk_sup:start_link().
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    erlangmk_sup:start_link().
 
 stop(_State) ->
-	ok.
--ifdef(TEST).
-app_test_() ->
-    [?_assertEqual(1, 2)].
--endif.
+    ok.
