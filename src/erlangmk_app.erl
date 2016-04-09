e75e6be81b0a3ebf7bf88ce6ed7f414c232342fb
yuanxiaopeng <fishballian@qq.com>
2016-04-09 14:28:18 +0800

    fff

--- src/erlangmk_app.erl
+++ src/erlangmk_app.erl
@@ -1,6 +1,8 @@
 -module(erlangmk_app).
 -behaviour(application).
-
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
 -export([start/2]).
 -export([stop/1]).
 
@@ -9,3 +11,7 @@ start(_Type, _Args) ->
 
 stop(_State) ->
 	ok.
+-ifdef(TEST).
+app_test_() ->
+    [?_assertEqual(1, 2)].
+-endif.
